a5e849f6c531bc0be16e9fe36174db668e23fa4a
cliff <cg_emers@live.nl>
2026-03-14 10:27:06 +0000

    update: finish Chapter03 vyos

--- Chapter03/VyOS/vyos_1.py
+++ Chapter03/VyOS/vyos_1.py
@@ -2,13 +2,21 @@
 
 import vymgmt
 
-vyos = vymgmt.Router('192.168.2.116', 'vyos', password='vyos')
+vyos = vymgmt.Router('192.168.0.24', 'vyos', password='vyos')
 vyos.login()
 vyos.configure()
 vyos.set("system domain-name networkautomationnerds.net")
 vyos.commit()
 vyos.save()
+
+raw_output = vyos.run_op_mode_command('show configuration | match domain-name')
+
+domain_name = ""
+for line in raw_output.splitlines():
+    if 'domain-name' in line:
+        domain_name = line.split()[1]
+
+print(domain_name)
+
 vyos.exit()
 vyos.logout()
-exit()
-
